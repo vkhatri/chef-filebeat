39bf17ff198d93c3e419194e380a665ba4384a5c
Prerak Shah <prerak.shah@clarabridge.com>
2016-04-18 09:59:54 -0400

    Fixed Travis errors.

--- attributes/default.rb
+++ attributes/default.rb
@@ -3,23 +3,21 @@ default['filebeat']['disable_service'] = false
 default['filebeat']['package_url'] = 'auto'
 
 default['filebeat']['notify_restart'] = true
-default['filebeat']['windows'] = {
-    'base_dir' => 'C:/opt/filebeat'
-}
+default['filebeat']['windows'] = {  'base_dir' => 'C:/opt/filebeat'}
 default['filebeat']['conf_dir'] = if node['platform'] == 'windows'
-                                     "#{node['filebeat']['windows']['base_dir']}/filebeat-#{node['filebeat']['version']}-windows"
+                                    "#{node['filebeat']['windows']['base_dir']}/filebeat-#{node['filebeat']['version']}-windows"
                                   else
-                                     '/etc/filebeat'
+                                    '/etc/filebeat'
                                   end
 default['filebeat']['conf_file'] = if node['platform'] == 'windows'
                                      "#{node['filebeat']['conf_dir']}/filebeat.yml"
-                                  else
+                                   else
                                      ::File.join(node['filebeat']['conf_dir'], 'filebeat.yml')
-                                  end
+                                   end
 default['filebeat']['prospectors_dir'] = if node['platform'] == 'windows'
-                                            "#{node['filebeat']['conf_dir']}/conf.d"
+                                           "#{node['filebeat']['conf_dir']}/conf.d"
                                          else
-                                            ::File.join(node['filebeat']['conf_dir'], 'conf.d')
+                                           ::File.join(node['filebeat']['conf_dir'], 'conf.d')
                                          end
 default['filebeat']['yum']['baseurl'] = 'https://packages.elastic.co/beats/yum/el/$basearch'
 default['filebeat']['yum']['description'] = 'Elastic Beats Repository'

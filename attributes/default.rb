2614513d7cee86ed9f5f205a6dd93495a11cdd8d
Virender Khatri <vir.khatri@gmail.com>
2016-09-09 17:18:05 +0530

    create separate attributes file for repository config

--- attributes/default.rb
+++ attributes/default.rb
@@ -10,21 +10,5 @@ default['filebeat']['solaris'] = {
   'manifest_directory' => '/var/svc/manifest/application'
 }
 
-default['filebeat']['yum']['baseurl'] = 'https://packages.elastic.co/beats/yum/el/$basearch'
-default['filebeat']['yum']['description'] = 'Elastic Beats Repository'
-default['filebeat']['yum']['gpgcheck'] = true
-default['filebeat']['yum']['enabled'] = true
-default['filebeat']['yum']['gpgkey'] = 'https://packages.elasticsearch.org/GPG-KEY-elasticsearch'
-default['filebeat']['yum']['metadata_expire'] = '3h'
-default['filebeat']['yum']['action'] = :create
-
-default['filebeat']['apt']['uri'] = 'https://packages.elastic.co/beats/apt'
-default['filebeat']['apt']['description'] = 'Elastic Beats Repository'
-default['filebeat']['apt']['components'] = %w(stable main)
-default['filebeat']['apt']['distribution'] = ''
-# apt package install options
-default['filebeat']['apt']['options'] = "-o Dpkg::Options::='--force-confnew'"
-default['filebeat']['apt']['action'] = :add
-default['filebeat']['apt']['key'] = 'https://packages.elasticsearch.org/GPG-KEY-elasticsearch'
 default['filebeat']['service']['retries'] = 0
 default['filebeat']['service']['retry_delay'] = 2

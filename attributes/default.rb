874c6311f0af06ff4010421109d34434db348e0e
Virender Khatri <vir.khatri@gmail.com>
2016-09-09 17:34:19 +0530

    update filebeat version to v1.3.0

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,4 +1,4 @@
-default['filebeat']['version'] = '1.2.3'
+default['filebeat']['version'] = '1.3.0'
 default['filebeat']['disable_service'] = false
 default['filebeat']['package_url'] = 'auto'
 default['filebeat']['package_force_overwrite'] = true

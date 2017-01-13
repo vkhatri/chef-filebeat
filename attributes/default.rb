dce7f3dfae140f4f0b2337df97f4eae97bdfd2ba
runningman84 <runningman84@users.noreply.github.com>
2017-01-13 15:45:13 +0100

    Update default.rb

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,4 +1,4 @@
-default['filebeat']['version'] = '5.1.1'
+default['filebeat']['version'] = '5.1.2'
 default['filebeat']['release'] = '1'
 default['filebeat']['disable_service'] = false
 default['filebeat']['package_url'] = 'auto'

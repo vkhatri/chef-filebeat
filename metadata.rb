5badf33afc0260bf9a86f68031966f19f291be37
Virender Khatri <vir.khatri@gmail.com>
2017-06-27 21:32:42 +0530

    corrected metadata chef_version [skip ci]

--- metadata.rb
+++ metadata.rb
@@ -7,7 +7,7 @@ long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version '0.5.0'
 source_url 'https://github.com/vkhatri/chef-filebeat' if respond_to?(:source_url)
 issues_url 'https://github.com/vkhatri/chef-filebeat/issues' if respond_to?(:issues_url)
-chef_version '~> 12'
+chef_version '>= 12' if respond_to?(:chef_version)
 
 depends 'windows'
 depends 'apt'

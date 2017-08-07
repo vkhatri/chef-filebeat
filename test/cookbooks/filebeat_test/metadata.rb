dfaed26df548cad46a12a4d43027647bf6c92027
Virender Khatri <vir.khatri@gmail.com>
2017-08-07 11:51:19 +0530

    added filebeat test cookbook

---
+++ test/cookbooks/filebeat_test/metadata.rb
@@ -0,0 +1,8 @@
+name 'filebeat_test'
+maintainer 'Virender Khatri'
+maintainer_email 'vir.khatri@gmail.com'
+license 'Apache 2.0'
+description 'Installs/Configures Filebeat Test Cookbook Resources'
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
+
+version '0.0.1'
---
+++ test/cookbooks/filebeat_test/recipes/default.rb
@@ -0,0 +1,11 @@
+filebeat_prospector 'test1' do
+  paths %w[/var/log/test1.log]
+  type 'log'
+  fields :type => 'test1_logs'
+end
+
+filebeat_prospector 'test2' do
+  paths %w[/var/log/test2.log]
+  type 'log'
+  fields :type => 'test2_logs'
+end

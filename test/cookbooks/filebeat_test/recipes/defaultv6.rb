8e05c1e949d4b2e3d5daf22c84ec1c7f8a44af45
Virender Khatri <vir.khatri@gmail.com>
2017-09-16 08:28:41 +0530

    add test recipe for v6 #120

---
+++ test/cookbooks/filebeat_test/recipes/defaultv6.rb
@@ -0,0 +1,47 @@
+filebeat_prospector 'test1' do
+  type 'log'
+  fields 'type' => 'test1_logs'
+  type 'log'
+  input_type 'log'
+  paths %w[/var/log/test1.log]
+  recursive_glob_enabled false
+  encoding 'utf-8'
+  exclude_lines ['^DBG']
+  include_lines ['^WARN', '^ERR']
+  exclude_files ['\.gz$']
+  tags ['kitchen']
+  fields 'engine' => 'kitchen'
+  fields_under_root true
+  ignore_older '1h'
+  close_inactive '1m'
+  close_renamed true
+  close_removed true
+  close_eof false
+  close_timeout '0'
+  clean_inactive '2h'
+  clean_removed true
+  scan_frequency '30s'
+  scan_sort 'modtime'
+  scan_order 'asc'
+  document_type 'log'
+  harvester_buffer_size 16_384
+  max_bytes 10_485_760
+  json_keys_under_root true
+  json_overwrite_keys false
+  json_add_error_key true
+  json_message_key 'log'
+  multiline_pattern '^\['
+  multiline_negate true
+  multiline_match 'after'
+  multiline_flush_pattern '^\[?[0-9]{10}\]'
+  multiline_max_lines 500
+  multiline_timeout '5s'
+  tail_files false
+  pipeline 'bigespipeline'
+  symlinks false
+  backoff '1s'
+  max_backoff '10s'
+  backoff_factor 2
+  harvester_limit 0
+  enabled true
+end

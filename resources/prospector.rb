69b496ec9e18eb0648bf63e901b008432bc5f330
Piotr Kantyka <piotr.kantyka@cognifide.com>
2018-10-30 09:46:47 +0100

    using property instead of attribute for prospector prefix

--- resources/prospector.rb
+++ resources/prospector.rb
@@ -7,6 +7,7 @@ resource_name :filebeat_prospector
 
 property :service_name, String, default: 'filebeat'
 property :filebeat_install_resource_name, String, default: 'default'
+property :prefix, String, default: 'lwrp-prospector-'
 property :config, [Array, Hash], default: {}
 property :cookbook_file_name, [String, NilClass], default: nil
 property :cookbook_file_name_cookbook, [String, NilClass], default: nil
@@ -16,7 +17,7 @@ property :config_sensitive, [TrueClass, FalseClass], default: false
 
 default_action :create
 
-prospector_prefix = node['filebeat']['prospector']['prefix']
+#prospector_prefix = node['filebeat']['prospector']['prefix']
 
 action :create do
   install_preview_resource = check_beat_resource(Chef.run_context, :filebeat_install_preview, new_resource.filebeat_install_resource_name)
@@ -41,7 +42,7 @@ action :create do
 
   if new_resource.cookbook_file_name && new_resource.cookbook_file_name_cookbook
     cookbook_file "prospector_#{new_resource.name}" do
-      path ::File.join(filebeat_install_resource.prospectors_dir, "#{prospector_prefix}#{new_resource.name}.yml")
+      path ::File.join(filebeat_install_resource.prospectors_dir, "#{prefix}#{new_resource.name}.yml")
       source new_resource.cookbook_file_name
       cookbook new_resource.cookbook_file_name_cookbook
       notifies :restart, "service[#{new_resource.service_name}]" if new_resource.notify_restart && !new_resource.disable_service
@@ -50,7 +51,7 @@ action :create do
     end
   else
     file "prospector_#{new_resource.name}" do
-      path ::File.join(filebeat_install_resource.prospectors_dir, "#{prospector_prefix}#{new_resource.name}.yml")
+      path ::File.join(filebeat_install_resource.prospectors_dir, "#{prefix}#{new_resource.name}.yml")
       content file_content
       notifies :restart, "service[#{new_resource.service_name}]" if new_resource.notify_restart && !new_resource.disable_service
       mode 0o600
@@ -62,7 +63,7 @@ end
 action :delete do
   filebeat_install_resource = find_beat_resource(Chef.run_context, :filebeat_install, new_resource.filebeat_install_resource_name)
   file "prospector_#{new_resource.name}" do
-    path ::File.join(filebeat_install_resource.prospectors_dir, "#{prospector_prefix}#{new_resource.name}.yml")
+    path ::File.join(filebeat_install_resource.prospectors_dir, "#{prefix}#{new_resource.name}.yml")
     action :delete
   end
 end
